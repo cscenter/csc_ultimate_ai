047ebdaffa40250be7b693434d3c549a6a2015b5
izhleba <izhleba@gmail.com>
2020-05-23 23:13:52 +0300

    - Fix: 200 also too large. Set 100.

--- web/app/competition.py
+++ web/app/competition.py
@@ -135,7 +135,7 @@ def run_competition():
                                                  "SERVER_URL": "*",
                                                  "SERVER_PORT": 4181,
                                                  "TOTAL_OFFER": 100,
-                                                 "TOTAL_ROUNDS": 200,
+                                                 "TOTAL_ROUNDS": 100,
                                                  "CLIENTS_AMOUNT": len(user_id_to_submissions),
                                                  "RESPONSE_TIMEOUT": 5,
                                                  "LOG_LEVEL": "debug"
